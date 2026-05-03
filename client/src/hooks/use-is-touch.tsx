6b1e59f0737be55df6945cced07ab8dfc3c31f64
Francesco Dammacco <francescodammacco@live.it>
2026-05-03 14:56:45 +0200

    Fix touch detection

--- client/src/hooks/use-is-touch.tsx
+++ client/src/hooks/use-is-touch.tsx
@@ -1,6 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function useIsTouch() {
-  const [isTouch] = useState('ontouchstart' in window || navigator.maxTouchPoints > 0)
+  const [isTouch, setIsTouch] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(pointer: coarse)')
+    setIsTouch(mediaQuery.matches)
+
+    function handleChange(e: MediaQueryListEvent) {
+      setIsTouch(e.matches)
+    }
+
+    mediaQuery.addEventListener('change', handleChange)
+    return () => mediaQuery.removeEventListener('change', handleChange)
+  }, [])
+
   return isTouch
 }
